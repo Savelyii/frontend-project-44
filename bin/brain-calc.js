85616eef18fd87184492d5c5cf5c1c770bfadf92
savelyii <leshaboichenko95@mail.ru>
2023-02-05 02:07:19 +0300

    transfer logic calc in src

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,50 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
-const Username = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${Username}!`);
-console.log('What is the result of the expression?');
-
-const getRandomNumber = (min, max) => {
-  const result = Math.floor(Math.random() * (max - min)) + min;
-  return result;
-};
-
-const startGameCalc = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber1 = getRandomNumber(1, 10);
-    const randomNumber2 = getRandomNumber(1, 10);
-    let correctAnswer = 0;
-    const arrSimbol = ['+', '-', '*'];
-    const getRandomSymbol = Math.floor(Math.random() * arrSimbol.length);
-    switch (arrSimbol[getRandomSymbol]) {
-      case '+':
-        correctAnswer = randomNumber1 + randomNumber2;
-        break;
-      case '-':
-        correctAnswer = randomNumber1 - randomNumber2;
-        break;
-      case '*':
-        correctAnswer = randomNumber1 * randomNumber2;
-        break;
-      default:
-        break;
-    }
-    correctAnswer = String(correctAnswer);
-    const expression = `${randomNumber1} ${arrSimbol[getRandomSymbol]} ${randomNumber2}`;
-    console.log(`Question: ${expression}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${Username}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${Username}!`);
-};
+import startGameCalc from '../src/games/calc.js';
 
 startGameCalc();
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,1 +1,32 @@
-console.log('Hello');
+#!/usr/bin/env node
+
+import { getRandomNumber, generalLogic } from '../index.js';
+
+const descriptionCalc = () => console.log('What is the result of the expression?');
+
+const logicGameCalc = () => {
+  const randomNumber1 = getRandomNumber(1, 10);
+  const randomNumber2 = getRandomNumber(1, 10);
+  let correctAnswer = 0;
+  const arrSimbol = ['+', '-', '*'];
+  const getRandomSymbol = Math.floor(Math.random() * arrSimbol.length);
+  switch (arrSimbol[getRandomSymbol]) {
+    case '+':
+      correctAnswer = randomNumber1 + randomNumber2;
+      break;
+    case '-':
+      correctAnswer = randomNumber1 - randomNumber2;
+      break;
+    case '*':
+      correctAnswer = randomNumber1 * randomNumber2;
+      break;
+    default:
+      break;
+  }
+  correctAnswer = String(correctAnswer);
+  const question = `${randomNumber1} ${arrSimbol[getRandomSymbol]} ${randomNumber2}`;
+  return [question, correctAnswer];
+};
+const startGameCalc = () => generalLogic(descriptionCalc, logicGameCalc);
+
+export default startGameCalc;
