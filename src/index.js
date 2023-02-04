12ef04c743229142c5261fe918953e26749acfc2
savelyii <leshaboichenko95@mail.ru>
2023-02-04 20:48:20 +0300

    push generallogic in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-const generationRandomnumber = (number) => Math.round(Math.random() * number);
+#!/usr/bin/env node
 
-export default generationRandomnumber;
+import readlineSync from 'readline-sync';
+
+export const getRandomNumber = (min, max) => {
+  const result = Math.floor(Math.random() * (max - min)) + min;
+  return result;
+};
+
+const generalLogic = (description, logicGame) => {
+  console.log('Welcome to the Brain Games!');
+  const Username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${Username}!`);
+  description();
+  for (let i = 0; i < 3; i += 1) {
+    const [question, correctAnswer] = logicGame();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${Username}`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${Username}!`);
+};
+
+export default generalLogic;
