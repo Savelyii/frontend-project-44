2903e8b7ea96d479f5dc3631721c33bbefcd2fe6
savelyii <leshaboichenko95@mail.ru>
2023-02-05 02:32:59 +0300

    transfer logic gcd in src

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,41 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
-const Username = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${Username}!`);
-console.log('Find the greatest common divisor of given numbers.');
-
-const getRandomNumber = (min, max) => {
-  const result = Math.floor(Math.random() * (max - min)) + min;
-  return result;
-};
-
-const gcdNumber = (num1, num2) => {
-  if (!num2) {
-    return num1;
-  }
-
-  return gcdNumber(num2, num1 % num2);
-};
-
-const startGameGcd = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const number1 = getRandomNumber(1, 11);
-    const number2 = getRandomNumber(1, 11);
-    console.log(`Question: ${number1} ${number2}`);
-    const answer = readlineSync.question('Your answer: ');
-    const correctAnswer = String(gcdNumber(number1, number2));
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${Username}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${Username}!`);
-};
+import startGameGcd from '../src/games/gcd.js';
 
 startGameGcd();
---
+++ src/games/gcd.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { getRandomNumber, generalLogic } from '../index.js';
+
+const descriptionGcd = () => console.log('Find the greatest common divisor of given numbers.');
+
+const logicGameGcd = () => {
+  const gcdNumber = (num1, num2) => {
+    if (!num2) {
+      return num1;
+    }
+
+    return gcdNumber(num2, num1 % num2);
+  };
+
+  const number1 = getRandomNumber(1, 11);
+  const number2 = getRandomNumber(1, 11);
+  const question = `${number1} ${number2}`;
+  const correctAnswer = String(gcdNumber(number1, number2));
+  return [question, correctAnswer];
+};
+
+const startGameGcd = () => generalLogic(descriptionGcd, logicGameGcd);
+
+export default startGameGcd;
