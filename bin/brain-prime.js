125ad4a58a8253c493969df022173a016c62c2e6
savelyii <leshaboichenko95@mail.ru>
2023-02-05 02:56:38 +0300

    transfer logic prime in scr

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,41 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
-const Username = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${Username}!`);
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const getRandomNumber = (min, max) => {
-  const result = Math.floor(Math.random() * (max - min)) + min;
-  return result;
-};
-
-const startGamePrime = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = getRandomNumber(2, 50);
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-    const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${Username}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${Username}!`);
-};
+import startGamePrime from '../src/games/prime.js';
 
 startGamePrime();
---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { getRandomNumber, generalLogic } from '../index.js';
+
+const descriptionPrime = () => console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+const logicGamePrime = () => {
+  const isPrime = (num) => {
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const question = getRandomNumber(2, 50);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
+const startGamePrime = () => generalLogic(descriptionPrime, logicGamePrime);
+
+export default startGamePrime;
